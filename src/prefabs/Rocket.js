ccef9458c1b17cd6d7d0d3274058246b7d974ca8
Arya Miryala <aryamiryala5@gmail.com>
2023-05-11 17:04:07 -0700

    randomized bone and rocket position

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,38 @@
+//bone prefab
+class Rocket extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        //add object to existing scene
+        scene.add.existing(this);
+        //pixels per frame
+       
+       //this.moveSpeed = speed; 
+    }
+
+    update() {
+        //move spaceship left
+        //console.log(this.x);
+        this.x -= 5; 
+        //console.log(this.x);
+        //wrap around from left edge to right edge
+        if(this.x <= 0 - this.width) {
+            this.x = game.config.width; 
+            //randomize spawn/direction 
+            //this.x = Math.floor(Phaser.Math.Between(500, game.config.width));
+            this.y = Math.floor(Phaser.Math.Between(borderUISize*12 + borderPadding*6, borderUISize*7 + borderPadding*9));
+            
+        
+        }
+
+       
+
+    }
+
+    //position reset
+    reset() {
+        //this.x = Math.floor(Math.random()); 
+        this.x = game.config.width; 
+
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,7 +6,13 @@ class Play extends Phaser.Scene {
         //load background image for play screen
         this.load.image('forest', './assets/forest1.png');
         this.load.image('ground', './assets/ground.png')
+        this.load.image('bone', './assets/bone.png');
+        this.load.image('rocket', './assets/rocket.png');
+        //load jump audio 
+        this.load.audio('jump','./assets/jump.wav');
         this.load.spritesheet('player', './assets/playersprite1.png', {frameHeight: 63, frameWidth: 125});
+        
+
         //this.load.atlas('player', './assets/playersprite1.png', './assets/playersprite.json');
        
 
@@ -14,50 +20,80 @@ class Play extends Phaser.Scene {
     create(){
          //add background image
          this.forest = this.add.tileSprite(0, 0, 640, 480, 'forest').setOrigin(0,0);
-         //this.player = this.physics.add.sprite(200, 200, 'player'); 
+         this.jumpAudio = this.sound.add('jump', {volume: 0.5});
+        
 
-        //  const ground = this.physics.add.staticGroup();
-
-        //  ground.create(200, 390, 'ground').setScale(2).refreshBody();
-         this.ground = this.physics.add.staticImage(200, 450, 'ground').setScale(2).refreshBody();
+       
+        this.ground = this.physics.add.staticImage(200, 450, 'ground').setScale(2).refreshBody();
 
         
 
-        this.createAnimations('run', 'player', 0, 10, -1, 10);
-        this.createAnimations('jump', 'player', 4, 4, -1, 10);
+        this.createAnimations('run', 'player', 0, 10, -1, 18);
+        this.createAnimations('jump', 'player', 4, 4, -1, 18);
+       
          
         this.player = this.physics.add.sprite(90, 560, 'player').setBounce(0.2).setCollideWorldBounds(true);
         this.player.play('run');
 
-       //this.player.setVelocityY(0);
 
+
+       
        this.physics.add.collider(this.player, this.ground);
 
+       //Bone
+       
+       this.bone1 = new Bone(this, game.config.width, borderUISize*12 + borderPadding*6, 'bone').setScale(0.12);
+       this.bone2 = new Bone(this, game.config.width, borderUISize*9 + borderPadding*7, 'bone').setScale(0.12);
+       this.bone3 = new Bone(this, game.config.width, borderUISize*7 + borderPadding*9, 'bone').setScale(0.12);
+       
+       this.rocket1 = new Rocket(this, game.config.width, borderUISize*9 + borderPadding*6, 'rocket').setScale(0.7);
+       //this.rocket2 = new Bone(this, game.config.width, borderUISize*12 + borderPadding*8, 'rocket').setScale(0.7);
 
 
        keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
+
+
         
-    //    this.add.sprite(200, 200).play('run');
 
     }
     update(){
-        this.forest.tilePositionX += 2; 
+        this.forest.tilePositionX += 3; 
+
+        this.bone1.update(); 
+        this.bone2.update(); 
+        this.bone3.update(); 
+
+        this.rocket1.update(); 
+        //this.rocket2.update(); 
+
+
        
 
+
+
+        
+       
+        //if player jumps
         if (Phaser.Input.Keyboard.JustDown(keySpace) && this.player.body.onFloor()) {
            
             // this.sound.play('sfx_select');
               this.player.setVelocityY(-160);
               this.player.play('jump');  
+              this.jumpAudio.play();
+    
               
            }
+           //check if player hits ground
            if(this.player.body.deltaY() > 0 && this.player.body.onFloor()){
             this.player.play('run');  
-          }
-           //this.player.setVelocityY(0);
+           }
+           
+          
         
     }
+
+    //animation function
     createAnimations(animKey, spriteKey, startFrame, endFrame, loopTimes, frameRate) {
         return (this.anims.create({
           key: animKey,
